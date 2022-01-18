b99acd953c36c70cf220c3f32a346dd2ae21019d
RHC0DE <58630159+RHC0DE@users.noreply.github.com>
2022-01-18 10:51:53 +0100

    Initial Commit

---
+++ SimpleBill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleBill
+//
+//  Created by Guest User on 18/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleBill/SimpleBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBillApp.swift
+//  SimpleBill
+//
+//  Created by Guest User on 18/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
